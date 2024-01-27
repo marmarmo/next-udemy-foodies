24e042d5fc81023a8fb1be4ed9c875e584b318ea
marmarmo <marla.roberts@thehoth.com>
2024-01-27 11:53:04 -0700

    adding styling to main header

--- src/components/main-header.js
+++ src/components/main-header.js
@@ -1,23 +1,26 @@
 import Link from 'next/link';
+import classes from './main-header.module.css';
 
 import logoImg from '@/assets/logo.png';
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/">
-            <img src={logoImg.src} alt="a plate with food on it" />
-            NextLevel Food
-        </Link>
+    return (
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <img src={logoImg.src} alt="a plate with food on it" />
+                NextLevel Food
+            </Link>
 
-        <nav>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Foodies Community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals">Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Foodies Community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
